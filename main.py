927aa01e2363e9f2100b7ee0ff08bc4be8a21c03
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-09-18 12:06:36 -0700

    IpPinger add some button commands

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 
 root = Tk()
 root.title("IP Pinger")
@@ -23,6 +24,23 @@ def helper():
     help_win.mainloop()
 
 
+def open_file():
+    text_file = filedialog.askopenfilename(initialdir="C:/Users/taner", title="Open file", filetypes=(("Text Files", "*.txt"),))
+    text_file = open(text_file, 'r')
+    rows = text_file.read()
+    upload_label.config(text=text_file.name)
+    uploaded_text.insert(END, rows)
+    text_file.close()
+
+
+def clear_uploaded_file():
+    uploaded_text.delete("1.0", "end")
+
+
+def clear_result_file():
+    result_text.delete("1.0", "end")
+
+
 main_menu = Menu(my_menu)
 my_menu.add_cascade(label="Main", menu=main_menu)
 main_menu.add_command(label="About", command=about)
@@ -33,35 +51,27 @@ main_menu.add_command(label="Exit", command=root.quit)
 upload_frame = LabelFrame(root, text="Upload .csv file", padx=30, pady=20)
 upload_frame.pack(padx=10, pady=10)
 
-upload_button = Button(upload_frame, text="Upload...")
+upload_button = Button(upload_frame, text="Upload...", command=open_file)
 upload_label = Label(upload_frame, text="           Path...")
-import_button = Button(upload_frame, text="Import")
-clear_button = Button(upload_frame, text="Clear")
-label_sep = Label(upload_frame, text="            ")
 upload_button.grid(row=0, column=0)
 upload_label.grid(row=0, column=2)
-label_sep.grid(row=1, column=0)
-label_sep.grid(row=2, column=0)
-label_sep.grid(row=3, column=0)
-label_sep.grid(row=4, column=0)
-import_button.grid(row=5, column=0)
-clear_button.grid(row=5, column=1)
+
 
 uploaded_frame = LabelFrame(root, text="Uploaded items", padx=30, pady=20)
 uploaded_frame.pack(padx=10, pady=10)
-uploaded_label = Label(uploaded_frame, text="IPs...", width=50)
+uploaded_text = Text(uploaded_frame, width=50)
 test_button = Button(uploaded_frame, text="Test IPs")
-clear_uploaded_button = Button(uploaded_frame, text="Clear")
-uploaded_label.grid(row=0, column=0)
+clear_uploaded_button = Button(uploaded_frame, text="Clear", command=clear_uploaded_file)
+uploaded_text.grid(row=0, column=0)
 test_button.grid(row=1, column=0)
 clear_uploaded_button.grid(row=1, column=1)
 
 result_frame = LabelFrame(root, text="Result", padx=30, pady=20)
 result_frame.pack(padx=10, pady=10)
-result_label = Label(result_frame, text="IPs...", width=50)
+result_text = Text(result_frame, width=50)
 export_button = Button(result_frame, text="Export file")
-clear_result_button = Button(result_frame, text="Clear")
-result_label.grid(row=0, column=0)
+clear_result_button = Button(result_frame, text="Clear", command=clear_result_file)
+result_text.grid(row=0, column=0)
 export_button.grid(row=1, column=0)
 clear_result_button.grid(row=1, column=1)
 root.mainloop()
