17f819b4ee8ddc59e41a1513722c2fd0af999eda
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-09-19 07:02:16 -0700

    IP Pinger add main option - test multiple ip addresses from file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+import os
 
 root = Tk()
 root.title("IP Pinger")
@@ -25,7 +26,7 @@ def helper():
 
 
 def open_file():
-    text_file = filedialog.askopenfilename(initialdir="C:/Users/taner", title="Open file", filetypes=(("Text Files", "*.txt"),))
+    text_file = filedialog.askopenfilename(initialdir="C:/Desktop", title="Open file", filetypes=(("Text Files", "*.txt"),))
     text_file = open(text_file, 'r')
     rows = text_file.read()
     upload_label.config(text=text_file.name)
@@ -39,6 +40,22 @@ def clear_uploaded_file():
 
 def clear_result_file():
     result_text.delete("1.0", "end")
+
+
+def test_ips():
+    line_list = uploaded_text.get('1.0', 'end').split('\n')
+    for line in line_list:
+        res = os.popen(f"ping -n 1 {line}").read()
+        if "Request timed out." in res:
+            result_text.insert(END, line + ' status: No' + '\n')
+        else:
+            result_text.insert(END, line + ' status: Yes' + '\n')
+    result_text.delete("end-2l", "end-1l")
+
+    with open('D:/readme.txt', 'w') as f:
+        line_list = result_text.get('1.0', 'end').split('\n')
+        for line in line_list:
+            f.write(line + "\n")
 
 
 main_menu = Menu(my_menu)
@@ -58,16 +75,16 @@ upload_label.grid(row=0, column=2)
 
 
 uploaded_frame = LabelFrame(root, text="Uploaded items", padx=30, pady=20)
-uploaded_frame.pack(padx=10, pady=10)
+uploaded_frame.pack(side=LEFT, padx=100, pady=10)
 uploaded_text = Text(uploaded_frame, width=50)
-test_button = Button(uploaded_frame, text="Test IPs")
+test_button = Button(uploaded_frame, text="Test IPs", command=test_ips)
 clear_uploaded_button = Button(uploaded_frame, text="Clear", command=clear_uploaded_file)
 uploaded_text.grid(row=0, column=0)
 test_button.grid(row=1, column=0)
 clear_uploaded_button.grid(row=1, column=1)
 
 result_frame = LabelFrame(root, text="Result", padx=30, pady=20)
-result_frame.pack(padx=10, pady=10)
+result_frame.pack(side=RIGHT, padx=100, pady=10)
 result_text = Text(result_frame, width=50)
 export_button = Button(result_frame, text="Export file")
 clear_result_button = Button(result_frame, text="Clear", command=clear_result_file)
---
+++ readme.txt
@@ -0,0 +1,6 @@
+10.137.192.86 status: No
+10.137.15.123 status: No
+10.137.192.37 status: No
+192.168.100.5 status: Yes
+
+
