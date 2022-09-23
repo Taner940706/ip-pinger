c93552f94370be184671015888ed751715449b01
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2022-09-23 12:31:25 -0700

    IP Pinger
    Added images for about form and icon
    Added in logs functionality - show result of pinging IPs
    Added functionality to show message box after test IPs and export them to the file

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from tkinter import *
 from tkinter import filedialog, ttk
+from tkinter import messagebox
+from tkinter import *
 import os
 
-root = Tk()
-root.title("IP Pinger")
-root.geometry("700x500")
 
-
-my_menu = Menu(root)
-root.config(menu=my_menu)
-
-pb = ttk.Progressbar(
-    root,
-    orient='horizontal',
-    mode='indeterminate',
-    length=280
-)
-pb.pack(side="bottom")
-
-
+# functions
 def about():
     about_win = Tk()
     about_win.title("About")
@@ -31,6 +17,17 @@ def helper():
     help_win.title("Help")
     help_win.geometry("700x500")
     help_win.mainloop()
+
+
+def show_logs():
+    log_win = Tk()
+    log_win.title("Logs")
+    log_win.geometry("700x500")
+    log_txt = Text(log_win, width=50)
+    for key in logs.keys():
+        log_txt.insert(INSERT, logs[key])
+    log_txt.pack()
+    log_win.mainloop()
 
 
 def open_file():
@@ -56,15 +53,14 @@ def test_ips():
     for line in line_list:
         res = os.popen(f"ping -n 1 {line}").read()
         if "Request timed out." in res:
-            x = line + ' status: No' + '\n'
-            result_text.insert(END, x)
+            result_text.insert(END, line + ' status: No' + '\n')
         else:
-            x = line + ' status: Yes' + '\n'
-            result_text.insert(END, x)
+            result_text.insert(END, line + ' status: Yes' + '\n')
+        logs[f'{line}'] = res
 
     pb.stop()
     result_text.delete("end-2l", "end-1l")
-
+    messagebox.showinfo('Test updated IP addresses', 'IP addresses are tested! ')
 
 
 def export_file():
@@ -72,43 +68,61 @@ def export_file():
         line_list = result_text.get('1.0', 'end').split('\n')
         for line in line_list:
             f.write(line + "\n")
+    messagebox.showinfo('Export file', 'File is exported in directory D:/readme.txt')
 
 
+# root
+root = Tk()
+root.title("IP Pinger")
+root.geometry("700x500")
+root.iconbitmap('./images/icon.ico')
+logs = {}
+
+# progress bar
+pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
+pb.pack(side="bottom")
+
+# menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
 main_menu = Menu(my_menu)
+log_menu = Menu(my_menu)
 my_menu.add_cascade(label="Main", menu=main_menu)
 main_menu.add_command(label="About", command=about)
 main_menu.add_command(label="Help", command=helper)
 main_menu.add_separator()
 main_menu.add_command(label="Exit", command=root.quit)
+my_menu.add_cascade(label='Logs', menu=log_menu)
+log_menu.add_command(label='Show logs', command=show_logs)
 
+# upload ip from file
 upload_frame = LabelFrame(root, text="Upload .csv file", padx=30, pady=20)
 upload_frame.pack(padx=10, pady=10)
-
 upload_button = Button(upload_frame, text="Upload...", command=open_file)
 upload_label = Label(upload_frame, text="           Path...")
 upload_button.grid(row=0, column=0)
 upload_label.grid(row=0, column=2)
 
-
+# imported file, test them
 uploaded_frame = LabelFrame(root, text="Uploaded items", padx=30, pady=20)
-myscrollbar = Scrollbar(uploaded_frame, orient="vertical")
-myscrollbar.pack(side="right", fill="y")
+scrollbar = Scrollbar(uploaded_frame, orient="vertical")
+scrollbar.pack(side="right", fill="y")
 uploaded_text = Text(uploaded_frame, width=50)
 test_button = Button(uploaded_frame, text="Test IPs", command=test_ips)
 clear_uploaded_button = Button(uploaded_frame, text="Clear", command=clear_uploaded_file)
-myscrollbar.config(command=uploaded_text.yview)
+scrollbar.config(command=uploaded_text.yview)
 uploaded_text.pack()
 test_button.pack()
 clear_uploaded_button.pack()
 uploaded_frame.pack(side=LEFT, padx=100, pady=10)
 
-
+# result of testing
 result_frame = LabelFrame(root, text="Result", padx=30, pady=20)
-myscrollbar_1 = Scrollbar(result_frame, orient="vertical")
-myscrollbar_1.pack(side="right", fill="y")
+scrollbar_1 = Scrollbar(result_frame, orient="vertical")
+scrollbar_1.pack(side="right", fill="y")
 result_text = Text(result_frame, width=50)
-myscrollbar_1.config(command=result_text.yview)
-export_button = Button(result_frame, text="Export file")
+scrollbar_1.config(command=result_text.yview)
+export_button = Button(result_frame, text="Export file", command=export_file)
 clear_result_button = Button(result_frame, text="Clear", command=clear_result_file)
 result_text.pack()
 export_button.pack()
